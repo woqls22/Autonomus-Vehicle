54dce7006b04c0845f23880f28c0dfe0b8c16027
LEEJAEBEEN <woqls226@gmail.com>
2020-07-12 23:15:22 +0900

    허프변환된 직선에 대해 필터링 적용

--- Scripts/Detection_test.py
+++ Scripts/Detection_test.py
@@ -12,31 +12,43 @@ height, width = (int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(capture.get(cv
 while True:
     if (capture.get(cv2.CAP_PROP_POS_FRAMES) == capture.get(cv2.CAP_PROP_FRAME_COUNT)):
         capture.open("./test_video.mp4")
-    retval, frame = capture.read()
-    cv2.imshow('Original Video', frame)
-    canny = LR.Canny(frame)
-    InterestArea = LR.InterestRegion(canny)
-    cv2.imshow('InterestArea', InterestArea)
-    line_arr = LR.Hough_lines(InterestArea, 1,1*np.pi/180,30,10,20) #허프변환
-    line_arr = np.squeeze(line_arr)
-    #gradient 도출
-    slope_degree = (np.arctan2(line_arr[:, 1] - line_arr[:, 3], line_arr[:, 0] - line_arr[:, 2]) * 180) / np.pi
-    # 수평 기울기 제한
-    line_arr = line_arr[np.abs(slope_degree) < 160]
-    slope_degree = slope_degree[np.abs(slope_degree) < 160]
-    # 수직 기울기 제한
-    line_arr = line_arr[np.abs(slope_degree) > 95]
-    slope_degree = slope_degree[np.abs(slope_degree) > 95]
-    # 필터링된 직선 버리기
-    L_lines, R_lines = line_arr[(slope_degree > 0), :], line_arr[(slope_degree < 0), :]
-    temp = np.zeros((frame.shape[0], frame.shape[1], 3), dtype=np.uint8)
-    L_lines, R_lines = L_lines[:, None], R_lines[:, None]
-    # 직선 그리기
-    LR.draw_lines(temp, L_lines)
-    LR.draw_lines(temp, R_lines)
-    result = LR.weighted_img(temp, frame)  # 원본 이미지에 검출된 선 overlap
-    cv2.imshow('result', result)  # 결과 이미지 출력
+    try:
+        retval, frame = capture.read()
+        cv2.imshow('Original Video', frame)
+        canny = LR.Canny(frame)
+        InterestArea = LR.InterestRegion(canny)
+        cv2.imshow('InterestArea', InterestArea)
+        line_arr = LR.Hough_lines(InterestArea, 1,1*np.pi/180,30,10,20) #허프변환
+        line_arr = np.squeeze(line_arr)
+        #gradient 도출
+        slope_degree = (np.arctan2(line_arr[:, 1] - line_arr[:, 3], line_arr[:, 0] - line_arr[:, 2]) * 180) / np.pi
+        # 수평 기울기 제한
+        line_arr = line_arr[np.abs(slope_degree) < 170]
+        slope_degree = slope_degree[np.abs(slope_degree) < 170]
+        # 수직 기울기 제한
+        line_arr = line_arr[np.abs(slope_degree) > 95]
+        slope_degree = slope_degree[np.abs(slope_degree) > 95]
+        # 필터링된 직선 버리기
+        L_lines, R_lines = line_arr[(slope_degree > 0), :], line_arr[(slope_degree < 0), :]
+        temp = np.zeros((frame.shape[0], frame.shape[1], 3), dtype=np.uint8)
+        L_lines, R_lines = L_lines[:, None], R_lines[:, None]
+        LeftLine = LR.get_representative(frame, L_lines)
+        RightLine = LR.get_representative(frame, R_lines)
 
+        # 직선 그리기
+        Lx1,Ly1,Lx2,Ly2 = LeftLine
+        Rx1,Ry1,Rx2,Ry2 = RightLine
+        Cx = Lx2+Rx2/2
+        print(Cx)
+        Cy = Ly2+Ry2/2
+        if(Cx<1050 and Cx>900and Lx2<Rx2):
+            LR.draw_lines(temp, LeftLine)
+            LR.draw_lines(temp, RightLine)
+
+        result = LR.weighted_img(temp, frame)  # 원본 이미지에 검출된 선 overlap
+        cv2.imshow('result', result)  # 결과 이미지 출력
+    except:
+        pass
 
     if cv2.waitKey(33) > 0: break
 
--- Scripts/LaneRecognition.py
+++ Scripts/LaneRecognition.py
@@ -14,11 +14,12 @@ def Canny(frame):
 def InterestRegion(frame):
     # Width 1280기준
     height = frame.shape[0]
-    area = np.array([[(300,height), (1200,height), (650,250)]]) # Area 지정
+    area = np.array([[(300,height), (1000,height), (650,250)]]) # Area 지정
     mask = np.zeros_like(frame)
     cv2.fillPoly(mask, area, 255)
     interestarea = cv2.bitwise_and(frame, mask)
     return interestarea
+
 
 def Hough_lines(interestregion, rho, theta, threshold, min_line_len, max_line_gap):
     lines = cv2.HoughLinesP(interestregion,rho,theta,threshold,np.array([]),minLineLength=min_line_len, maxLineGap=max_line_gap)
@@ -30,6 +31,17 @@ def weighted_img(img, initial_img, a=1, b=1., l=0.): # 두 이미지 operlap 하
     return cv2.addWeighted(initial_img, a, img, b, l)
 
 def draw_lines(frame, lines, color=[0, 255, 0], thickness=2): # 선 그리기
-    for line in lines:
-        for x1,y1,x2,y2 in line:
-            cv2.line(frame, (x1, y1), (x2, y2), color, thickness)
+    cv2.line(frame, (lines[0], lines[1]), (lines[2], lines[3]), color, thickness)
+
+
+def get_representative(frame, lines):  # Representative Line 구하기
+    lines = np.squeeze(lines)
+    lines = lines.reshape(lines.shape[0] * 2, 2)
+    rows, cols = frame.shape[:2]
+    output = cv2.fitLine(lines, cv2.DIST_L2, 0, 0.01, 0.01)
+    vx, vy, x, y = output[0], output[1], output[2], output[3]
+    x1, y1 = int(((frame.shape[0] - 1) - y) / vy * vx + x), frame.shape[0] - 1
+    x2, y2 = int(((frame.shape[0] / 2 + 100) - y) / vy * vx + x), int(frame.shape[0] / 2 + 100)
+
+    result = [x1, y1, x2, y2]
+    return result
